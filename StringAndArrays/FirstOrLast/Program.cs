ace260e232fe659b3c23b858c0465654233b6369
Kevin <kkivikas@tktk.com>
2021-10-09 16:08:27 +0300

    sõned ja massivid

---
+++ StringAndArrays/FirstOrLast/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FirstOrLast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada ees-ja perekonnanimed
+            //programm kontrollib andmete pikkust 
+            //programm kuvab kumb nendest on pikem, kas ees-või perekonnanimi
+
+            Console.WriteLine("sisesta eesnimi: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("siesta perekonnanimi");
+            string lastName = Console.ReadLine();
+
+            int firstNameLength = firstName.Length;
+            int lastNameLength = lastName.Length;
+
+            if(firstNameLength > lastNameLength){
+                Console.WriteLine("Teie eesnimi on pikem");
+            }
+            else if(lastNameLength > firstNameLength){
+
+                Console.WriteLine("Teie perekonnanimi on pikem");
+            }
+            else
+            {
+                Console.WriteLine("Teie eesnimi ja perekonnanimi on sama pikad");
+            }
+
+        }
+    }
+}
---
+++ StringAndArrays/HOLCounter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HOLCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm kuvab konsoolis, mitu "h", "o" ja "l" tähte on lauses "Hello World!"           
+            string lause = "Hello World!".ToLower();   
+            int hCounter = 0;
+            int oCounter = 0;
+            int lCounter = 0;
+
+            for (int i = 0; i < lause.Length; i++)
+            {
+                if (lause[i] == 'h')
+                {
+                    hCounter++;
+                }
+                if (lause[i] == 'o')
+                {
+                    oCounter++;
+                }
+                if (lause[i] == 'l')
+                {
+                    lCounter++;
+                }
+            }
+
+             Console.WriteLine($"Sinu lauses on {hCounter} 'h' tähte");
+             Console.WriteLine($"Sinu lauses on {lCounter} 'l' tähte");
+             Console.WriteLine($"Sinu lauses on {oCounter} 'o' täht");
+           
+        }
+    }
+}
